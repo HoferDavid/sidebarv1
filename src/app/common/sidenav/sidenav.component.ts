65ca6c7c0f6dab8849ea623539441e98cb838563
David Hofer <chassis.poppets0x@icloud.com>
2024-09-23 18:38:19 +0200

    Sidenav Elements

--- src/app/common/sidenav/sidenav.component.ts
+++ src/app/common/sidenav/sidenav.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: any;
+}
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, RouterModule],
+  imports: [
+    MatSidenavModule,
+    RouterModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
-
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard',
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content',
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytics',
+      route: 'analytics',
+    },
+    {
+      icon: 'comment',
+      label: 'Comment',
+      route: 'comment',
+    },
+  ]);
 }
